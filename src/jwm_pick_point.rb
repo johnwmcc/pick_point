0fcd0f782db1c7bc1c5b7eba6e4ec700989b6896
John McClenahan <john.mcclenahan@gmail.com>
2014-04-05 13:42:56 +0100

    Corrected typos and missing info in extension registration

--- src/jwm_pick_point.rb
+++ src/jwm_pick_point.rb
@@ -1,10 +1,8 @@
-﻿# Copyright 2014 Trimble Navigation Ltd.
+﻿# Copyright 2014 Trimble Navigation Ltd and John McClenahan.
 #
 # License: The MIT License (MIT)
 #
-# A SketchUp Ruby Extension that creates simple shape objects.  More info at
-# https://github.com/SketchUp/shapes
-
+# A SketchUp Ruby Extension to pick an origin point, preparatory to creating  shape objects.  
 
 require "sketchup.rb"
 require "extensions.rb"
@@ -16,12 +14,12 @@ module CommunityExtensions
     loader = File.join(File.dirname(__FILE__), "jwm_pick_point", "pick_point.rb")
     extension = SketchupExtension.new("Pick Point", loader)
     extension.description = "Pickpoint script"
-    extension.version     = "1.0"
+    extension.version     = "1.04"
     extension.creator     = "John McClenahan"
     extension.copyright   = "2014, Trimble Navigation Limited and " <<
                             "John W McClenahan"
 
-    # Register the extension with so it show up in the Preference panel.
+    # Register the extension with so it shows up in the Preference panel.
     Sketchup.register_extension(extension, true)
 
   end # module PickPoint
